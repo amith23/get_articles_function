6bf078aab386facfa7f3c1b49c9e1ad83670f24d
amith <amithdarshana@gmail.com>
2023-01-22 16:35:08 +0530

    first commit

---
+++ article_get/src/main/java/com/example/article_get/ArticleGetApplication.java
@@ -0,0 +1,13 @@
+package com.example.article_get;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleGetApplication.class, args);
+	}
+
+}
