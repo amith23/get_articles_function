4b40eb13fa3af22983cb0898204484964f2c1780
amith <amithdarshana@gmail.com>
2023-01-22 16:51:53 +0530

    add some codes

--- article_get/src/main/java/com/example/article_get/ArticleGetApplication.java
+++ article_get/src/main/java/com/example/article_get/ArticleGetApplication.java
@@ -1,13 +1,25 @@
 package com.example.article_get;
 
-import org.springframework.boot.SpringApplication;
+import java.util.function.Function;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArticleGetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArticleGetApplication.class, args);
+		// SpringApplication.run(ArticleGetApplication.class, args);
 	}
 
+	@Bean
+	public Function<String, String> uppercase() {
+		return value -> {
+			if (value.equals("exception")) {
+				throw new RuntimeException("Intentional exception");
+			} else {
+				return value.toUpperCase();
+			}
+		};
+	}
 }
