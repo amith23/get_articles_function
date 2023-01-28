ea5104cd63e1858e5140a32c21b75b4b9fca2ff0
amith <amithdarshana@gmail.com>
2023-01-28 11:14:00 +0530

    add request handler

---
+++ src/main/java/com/example/article_get/MyStringHandlers.java
@@ -0,0 +1,7 @@
+package com.example.article_get;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
+
+}
